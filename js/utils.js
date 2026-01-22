74dbad9a8c79cf4ee26b8a75a8ff130fea5dee4b
euripedesjaveras-max <euripedesjaveras@gmail.com>
2026-01-22 13:05:14 -0300

    Update utils.js
    v1

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,9 @@
+function getUnidadeFromURL() {
+  const params = new URLSearchParams(window.location.search);
+  return params.get("unidade");
+}
+
+function fetchJSON(path) {
+  return fetch(path).then(r => r.json());
+}
 
